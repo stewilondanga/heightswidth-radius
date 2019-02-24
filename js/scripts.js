3205fb1cce8b960a8b592d43d1908f9161419476
Stewart ilondanga <stilondanga@gmail.com>
2019-02-24 12:52:29 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
-/*var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+var navigate = (function() {
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
 
-var props = {
+/*var props = {
   'border-radius': `
     Sets the radius of an element to the specified amount.<br><br>
     Example:<br>
